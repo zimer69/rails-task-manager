5caea764dffd760227892f6b03676cc262ff2122
Dimitri Zimermann <dimitrizimer@gmail.com>
2022-08-16 16:51:46 -0300

    working with rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+#   get 'tasks', to: 'tasks#index'
+#   get 'tasks/new', to: 'tasks#new'
+#   get 'tasks/:id', to: 'tasks#show', as: :task
+#   post 'tasks', to: 'tasks#create'
+#   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+#   patch 'tasks/:id', to: 'tasks#update'
+#   delete 'tasks/:id', to: 'tasks#destroy'
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+# end
+
+Rails.application.routes.draw do
+  resources :tasks
 end
---
+++ db/migrate/20220816151702_set_default_completed_in_tasks.rb
@@ -0,0 +1,5 @@
+class SetDefaultCompletedInTasks < ActiveRecord::Migration[7.0]
+  def change
+    change_column :tasks, :completed, :boolean, default: false
+  end
+end
